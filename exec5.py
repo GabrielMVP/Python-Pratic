eadb4972eda750431ec1fb86d9e6d21ddc34ede9
Gabriel Moreno Cavalcanti <38257470+GabrielMVP@users.noreply.github.com>
2024-02-25 12:08:04 -0300

    Add files via upload

---
+++ exec5.py
@@ -0,0 +1,7 @@
+salario_minimo = 1293.20
+
+pergunta = float(input("Qual o valor do seu salário mínimo?"))
+
+divisao = pergunta / salario_minimo
+
+print("Você recebe:", divisao, "Salários mínimos.")
