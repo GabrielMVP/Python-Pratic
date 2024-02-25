7a66d521dfb095562e1b47320814c8fb92d7a26e
Gabriel Moreno Cavalcanti <38257470+GabrielMVP@users.noreply.github.com>
2024-02-24 22:15:16 -0300

    Add files via upload

---
+++ exec3.py
@@ -0,0 +1,12 @@
+numA = int(input('Digite o valor de A: '))
+
+numB = int(input('Digite o valor de B: '))
+
+soma = numA + numB
+
+multiplicacao = numA * numB
+
+if numA == numB:
+    print('Os números A e B são iguais,', "O valor de C é: ",soma)
+else:
+    print('Os números A e B são diferentes,', "O valor de C é: ", multiplicacao)
