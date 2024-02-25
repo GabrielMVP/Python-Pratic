338c871263c66093b029eeb56d0a18ef59968923
Gabriel Moreno Cavalcanti <38257470+GabrielMVP@users.noreply.github.com>
2024-02-24 22:24:11 -0300

    Add files via upload

---
+++ exec4.py
@@ -0,0 +1,9 @@
+numA = int(input('Digite um número qualquer: '))
+
+soma = numA + 1
+
+subtracao = numA - 1
+
+print('O valor que você escolheu foi:', numA)
+print('O seu antecessor é:', subtracao)
+print('O seu sucessor é:', soma)
