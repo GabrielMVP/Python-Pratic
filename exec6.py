e3bccfb345342c079f73e6cd0503e81a8425d909
Gabriel Moreno Cavalcanti <38257470+GabrielMVP@users.noreply.github.com>
2024-02-25 12:16:26 -0300

    Add files via upload

---
+++ exec6.py
@@ -0,0 +1,9 @@
+pergunta = float(input("Escolha um valor qualquer:"))
+
+ajuste = pergunta * 0.05
+
+reajuste = pergunta + ajuste
+
+print("O valor escolhido foi de:", pergunta)
+print("O valor do ajuste foi de:", ajuste)
+print("O valor do reajuste foi de:", reajuste)
